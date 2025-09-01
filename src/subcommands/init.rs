42f2307eb53d996a94b7565f3ebff6c525da8824
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2025-09-01 10:57:44 +0200

    refactor(subcommands/init.rs): make creation more streamlined

--- src/subcommands/init.rs
+++ src/subcommands/init.rs
@@ -8,19 +8,22 @@ use std::path::PathBuf;
 pub fn run(matches: &ArgMatches) -> std::io::Result<()> {
 	let destination = matches.get_one::<String>("destination").unwrap();
 	println!("Generating init files at {}...", destination);
-	match add_to_dir(destination) {
+
+	match add_file_to_dir(destination, "default.yaml", DEF_YAM) {
 		Ok(_) => println!("Init files added to the chosen directory!"),
-		Err(e) => println!("Error while adding to directory: {}", e),
+		Err(e) => println!("Error while adding default.yaml to directory: {}", e),
 	}
+	match add_file_to_dir(destination, "default.csv", DEF_CSV) {
+		Ok(_) => println!("Init files added to the chosen directory!"),
+		Err(e) => println!("Error while adding default.csv to directory: {}", e),
+	}
+
 	Ok(())
 }
 
-fn add_to_dir(destination: &String) -> std::io::Result<()> {
-	let yaml_path = PathBuf::from(destination).join("default.yaml");
-	let mut yaml_file = File::create(yaml_path)?;
-	yaml_file.write_all(DEF_YAM.as_bytes())?;
-	let csv_path = PathBuf::from(destination).join("default.csv");
-	let mut csv_file = File::create(csv_path)?;
-	csv_file.write_all(DEF_CSV.as_bytes())?;
+fn add_file_to_dir(destination: &str, filename: &str, contents: &str) -> std::io::Result<()> {
+	let path = PathBuf::from(destination).join(filename);
+	let mut file = File::create(path)?;
+	file.write_all(contents.as_bytes())?;
 	Ok(())
 }
